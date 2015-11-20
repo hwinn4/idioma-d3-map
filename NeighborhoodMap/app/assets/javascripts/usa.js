33005c191c10ebf3644fb1b7855e9e5c53956dbc
hwinn4 <hilary.winn@gmail.com>
2015-11-19 21:41:38 -0500

    tests text display

--- NeighborhoodMap/app/assets/javascripts/usa.js
+++ NeighborhoodMap/app/assets/javascripts/usa.js
@@ -9,22 +9,23 @@ $(function(){
 
     var path = d3.geo.path();
 
+    var color = d3.scale.category20b();
+
     var g = svg.append("g");
 
     // path data
     d3.json("assets/us.json", function(unitedState) {
       var data = topojson.feature(unitedState, unitedState.objects.states).features;
+      neighbors = topojson.neighbors(unitedState.objects.states.geometries);
       // our names
       d3.tsv("assets/us-state-names.tsv", function(tsv){
-        // extract just the names and Ids
+        // create hash of the names and ids
         var names = {};
         tsv.forEach(function(d,i){
           names[d.id] = d.name;
         });
-   
     
-
-
+   
     // build paths
     g.append("g")
       .attr("class", "states-bundle")
@@ -33,6 +34,7 @@ $(function(){
       .enter()
       .append("path")
       .attr("d", path)
+      .style("fill", function(d, i) { return color(d.color = d3.max(neighbors[i], function(n) { return data[n].color; }) + 1 | 0); })
       .attr("stroke", "white")
       .attr("class", "states");
 
@@ -55,5 +57,10 @@ $(function(){
       .attr("text-anchor","middle")
       .attr('fill', 'white');
          })
-     })  
+     })
+
+    
 });
+
+// END MAP
+
