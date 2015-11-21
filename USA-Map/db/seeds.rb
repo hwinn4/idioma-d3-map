a0e5f31fc7484348837ba8ea85c029ed5c9d063c
hwinn4 <hilary.winn@gmail.com>
2015-11-21 15:22:21 -0500

    completes seed file

--- USA-Map/db/seeds.rb
+++ USA-Map/db/seeds.rb
@@ -8,11 +8,17 @@
 
 # require_relative '../app/models/adapters/data_parser.rb'
 
+State.delete_all
+Language.delete_all
+StateLanguage.delete_all
+
 Adapters::DataParser.parse.each do |state_hash|
-  new_state = State.create(name: state_hash["state"])
+
+  new_state = State.find_or_create_by(name: state_hash["state"])
   first_language = Language.find_or_create_by(name: state_hash["language1"])
-  StateLanguage.create(state_id: new_state.id, language_id: first_language.id, num_speakers: state_hash["speakers1"].to_i)
-  byebug
-  second_language = Language.find_or_create_by(name: state_hash["language1"])
-  StateLanguage.create(state_id: new_state.id, language_id: second_language.id, num_speakers: state_hash["speakers2"].to_i)
+  StateLanguage.find_or_create_by(state_id: new_state.id, language_id: first_language.id, num_speakers: state_hash["speakers1"].to_i)
+
+  second_language = Language.find_or_create_by(name: state_hash["language2"])
+  StateLanguage.find_or_create_by(state_id: new_state.id, language_id: second_language.id, num_speakers: state_hash["speakers2"].to_i)
+
 end
