da273990fa928a52be7ab42189848722a341aafb
hwinn4 <hilary.winn@gmail.com>
2015-11-22 11:19:09 -0500

    fixes bug-- can now click on state to display state data

--- USA-Map/app/controllers/state_languages_controller.rb
+++ USA-Map/app/controllers/state_languages_controller.rb
@@ -7,7 +7,7 @@ class StateLanguagesController < ApplicationController
     state_name = state_name.join(" ")
     @state = State.find_by(name: state_name)
     @state_languages = @state.state_languages
-    string = render_to_string(partial: "state_languages/data", locals: {:language_info => @state_languages})
+    string = render_to_string(partial: "state_languages/data", locals: {:state => @state, :language_info => @state_languages})
     render json: {text: string}
   end
 
