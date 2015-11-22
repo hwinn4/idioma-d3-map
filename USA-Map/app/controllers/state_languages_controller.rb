70ba594634bdbac3565f36cd9602ef76cad67add
hwinn4 <hilary.winn@gmail.com>
2015-11-21 21:21:58 -0500

    displays data about languages when you click on a state

--- USA-Map/app/controllers/state_languages_controller.rb
+++ USA-Map/app/controllers/state_languages_controller.rb
@@ -6,18 +6,7 @@ class StateLanguagesController < ApplicationController
   def show
     @state = State.find_by(name: params["name"])
     @state_languages = @state.state_languages
-    # @state_languages = @state.languages
-    byebug
     string = render_to_string(partial: "state_languages/data", locals: {:state => @state, :language_info => @state_languages})
     render json: {text: string}
-    # respond_to do |format|
-    #   if @state_languages
-    #     format.html { redirect_to @state, notice: 'State info found.' }
-    #     format.json { render :show, status: :created, location: @state }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @state.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 end
