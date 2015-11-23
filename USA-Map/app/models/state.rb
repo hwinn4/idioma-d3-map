0cb9ee01f50a31b68dbca1411505057fba2a9fcc
hwinn4 <hilary.winn@gmail.com>
2015-11-23 10:54:31 -0500

    adds jumbotron div with welcome message

--- USA-Map/app/models/state.rb
+++ USA-Map/app/models/state.rb
@@ -28,23 +28,27 @@ class State < ActiveRecord::Base
 
 
    def self.language_colors
-    {
-      "Spanish" => "aqua", 
-      "German" => "blueviolet", 
-      "Yupik" => "coral", 
-      "Navajo" =>  "darkcyan", 
-      "Tagalog" => "darkolivegreen", 
-      "Polish" => "darkseagreen",
-      "Chinese" => "cornflowerblue",
-      "French Creole" => "indigo", 
-      "Korean" => "lawngreen", 
-      "Japanese" => "lightsteelblue", 
-      "French" => "maroon", 
-      "Portuguese" => "peru", 
-      "Arabic" => "darkorange", 
-      "Hmong" => "tan", 
-      "Vietnamese" => "yellowgreen", 
-      "Dakota" => "hotpink"
+    # dark orange, light orange, neon green, 
+    # light blue, med blue, dark blue,
+    # pink-purple, purple, teal
+    # green, brown, tan, 
+    colors = {
+      "Spanish" => "rgb(74,217,217)", 
+      "German" => "rgb(4,117,111)", 
+      "Yupik" => "rgb(147,255,0)", 
+      "Navajo" =>  "rgb(7,219,255)", 
+      "Tagalog" => "rgb(7,118,255)", 
+      "Polish" => "rgb(6,16,255)",
+      "Chinese" => "rgb(206,5,255)",
+      "French Creole" => "rgb(127,60,153)", 
+      "Korean" => "rgb(92,78,175)", 
+      "Japanese" => "rgb(255,141,0)", 
+      "French" => "rgb(148,9,13)", 
+      "Portuguese" => "rgb(70,137,102)", 
+      "Arabic" => "rgb(167,197,32)", 
+      "Hmong" => "rgb(255,243,0)", 
+      "Vietnamese" => "rgb(255,72,0)", 
+      "Dakota" => "rgb(246,177,195)"
     }
   end
  
